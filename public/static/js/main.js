fd20fff1af47a82c934cc4542746791b7d55be4f
Pascal Wittler <git@pascalwittler.de>
2024-05-24 22:24:03 +0200

    Fetch single book recommendation articles + colorize tiles via javascript

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -3,7 +3,18 @@ const phpApiUrl = 'book-recommendations.php';
 class BookRecommendationsParser {
   constructor() {
     fetch(phpApiUrl)
-      .then((response) => response.json());
+      .then((response) => response.json())
+      .then((data) => {
+        const tiles = Array.from(document.querySelectorAll('.book-recommendation'));
+
+        Array.from(data.linkList).map((link, index) => {
+          fetch(phpApiUrl + '?article=' + link)
+            .then((response) => response.json())
+            .then((data) => {
+              tiles[index].classList.add(data.bookRecommendation.includesLucie ? 'positive' : 'negative');
+            });
+        });
+      });
   }
 }
 
