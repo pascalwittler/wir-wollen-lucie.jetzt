4f98524aeb9c274d465f76279868ba01b1500f25
Pascal Wittler <git@pascalwittler.de>
2024-05-24 21:42:30 +0200

    Add `renderJson()` to `BookRecommendation` api

--- public/book-recommendations.php
+++ public/book-recommendations.php
@@ -2,6 +2,11 @@
 
 class BookRecommendations
 {
+    protected function renderJson($data)
+    {
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($data);
+    }
 }
 
 new BookRecommendations();
